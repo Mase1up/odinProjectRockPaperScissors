1e4bbea9a93909c557e0d275730f51383b7ce3e1
Mase1up <mason.l.keith@gmail.com>
2024-07-07 12:28:43 -0400

    Not recording scores now

--- index.js
+++ index.js
@@ -1,51 +1,64 @@
-// Already coded the main part on the laptop, but it's having issues
-// and the git commits I did didn't push.  I need to see how to fix that.
-// Never tried using git from the GUI before, need to learn
+// Don't know why, but start button is staying clicked when page loads, not when clicked
+// const startButtonEl = document.getElementById("start-game-btn");
+// startButtonEl.addEventListener("click", console.log("Start game clicked"));
 
-// 1. Create function getComputerChoice that returns Rock, Paper, or Scissors randomly
+let playerScore = 0;
+let computerScore = 0;
+
+let playerScoreStatement = `Player Score: ${playerScore}`;
+let computerScoreStatement = `CPU Score: ${computerScore}`;
+
+let computerChoice
+let playerChoice
+
+function bestOfFiveGames() {
+    for (let i = 0; playerScore < 3 && computerScore < 3; i++) {
+        let playerScoreTally
+        let computerScoreTally
+        startGame();
+
+    }
+}
+
+function startGame() {
+    getChoices();
+    playRound(playerChoice, computerChoice);
+}
+
+
+function getChoices() {
+    getComputerChoice();
+    getPlayerChoice();
+}
 
 function getComputerChoice() {
     // Generate random number 1 to 3
     let number = Math.floor(Math.random() * 3) + 1;
     // Assign a selection to each integer
     if (number === 1) {
-        return "ROCK";
+        computerChoice = "ROCK";
     } else if (number === 2) {
-        return "PAPER"
+        computerChoice = "PAPER";
     } else {
-        return "SCISSORS"
+        computerChoice = "SCISSORS";
     }
 }
-// console.log(getComputerChoice()); // Test it
-// console.log(playGame("paper", getComputerChoice())); // Test updated version
-
-
-
-const computerChoice = getComputerChoice();
-
-// Want to establish variable as upper-case version of Rock, Paper, or Scissors, as typed by user
-// Now that I moved the prompt inside the function 
-let playerChoice = getPlayerChoice();
 
 function getPlayerChoice() {
     let userInput = prompt("Rock, Paper, or Scissors?");
     if (userInput.toUpperCase() === 'ROCK' || userInput.toUpperCase() === 'PAPER' || userInput.toUpperCase() === 'SCISSORS') {
-        console.log("Successful prompt input");
-        return userInput.toUpperCase();
+        playerChoice = userInput.toUpperCase();
     } else {
         getPlayerChoice();
     }
 }
 
-console.log(playerChoice);
-
 // Write a function that player a round against the computer, accepting 2 arguments
 // playerSelection and computerSelection.  Returns a string declaring the winner.
 // Inputs should be case insensitive
+// I had to change returns to console.log for now
 
-function playGame(playerSelection, computerSelection) {
-    // Set playerSelection to uppercase so it's case insensitive
-    playerSelection = playerSelection.toUpperCase();
+function playRound(playerSelection, computerSelection) {
     // Set my statements to return based on the follow-up logic
     let playerWins = `You win!  ${playerSelection} beats ${computerSelection}.`;
     let playerLoses = `Sorry, you lost.  ${computerSelection} beats ${playerSelection}.`
@@ -54,30 +67,51 @@ function playGame(playerSelection, computerSelection) {
     // Logic to decide which statement to return
     if (playerSelection === "ROCK") {
         if (computerSelection === "ROCK") {
-            return playerTies;
+            console.log(playerTies);
+            printScore();
         } else if (computerSelection === "PAPER") {
-            return playerLoses;
+            console.log(playerLoses);
+            printScore();
         } else {
-            return playerWins;
+            playerScore++;
+            console.log(playerWins);
+            printScore();
         }
     } else if (playerSelection === "PAPER") {
         if (computerSelection === "ROCK") {
-            return playerWins;
+            playerScore++;
+            console.log(playerWins);
+            printScore();
         } else if (computerSelection === "PAPER") {
-            return playerTies;
+            console.log(playerTies);
+            printScore();
         } else {
-            return playerLoses;
+            computerScore++;
+            console.log(playerLoses);
+            printScore();
         }
     } else {
         if (computerSelection === "ROCK") {
-            return playerLoses;
+            computerScore++;
+            console.log(playerLoses);
+            printScore();
         } if (computerSelection === "PAPER") {
-            return playerWins;
+            playerScore++;
+            console.log(playerWins);
+            printScore();
         } else {
-            return playerTies;
+            console.log(playerTies);
+            printScore();
         }
     }
 }
+
+function printScore() {
+    console.log(playerScoreStatement);
+    console.log(computerScoreStatement);
+}
+
+bestOfFiveGames();
 
 // Write a new function called game().  Use the previous function inside this one
 // to play a 5 round game and declare a winner at the end.
